87184917d9f90e8c474cf568d71b07ad2c2a08d2
divyansh kr. chaturvedi <divyanshchaturvedi317@gmail.com>
2024-02-13 03:31:35 +0530

    adding new files

---
+++ Thapa node js/01 fs module SYNCHRONOUSLY.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+ // fs.writeFileSync("read.txt","welcome to fs module learning");
+// fs.appendFileSync("read.txt","welcome to fs module learning , lets start ! ");
+
+
+// const buf_data = fs.readFileSync("read.txt" );     // reading the data
+// console.log(buf_data);                           // u know node js has a extra data buffer which store the data in binary format 
+
+// org_data = buf_data.toString();                // convert the buffer data into string 
+// console.log(org_data);
+
+// fs.renameSync('readWrite.txt','read.txt'); // rename the read file 
+
+// const buf_data1 = fs.readFileSync("read.txt","utf8");     // reading the data without creating buffer 
+// console.log(buf_data1);   
+
+
+fs.unlinkSync("read.txt");
---
+++ Thapa node js/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+fs.readFile('readWrite.txt','utf8',(err,data) => {   // to read the file directly into a string or another type without explicitely creating a buffer.
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// });
